031d1fc569f03a53ddbdb1f3ca81bc122a0d5007
Benjamin Loughmiller <ben.loughmiller3218@gmail.com>
2024-10-01 09:44:54 -0600

    Completed Base version of the game

--- TaxCollector/obj/Debug/net8.0/TaxCollector.AssemblyInfo.cs
+++ TaxCollector/obj/Debug/net8.0/TaxCollector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaxCollector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63361bee541028dcfbaa8dbee6a4d5b6142bd243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eee06e7ad883a924c8cb594744ebcd1a23667bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaxCollector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaxCollector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
